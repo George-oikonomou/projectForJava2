7d43d158d7d5a9f830da6dbf5d7455a1324af60d
George-oikonomou <giorgos@workadu.com>
2023-11-07 01:30:28 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static int calculate(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int result = calculate(5, 3);
+        System.out.println("Result: " + result);
+    }
+}
