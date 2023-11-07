98f29b298abe1fad568d9c0d42a233876af4f35d
George-oikonomou <giorgos@workadu.com>
2023-11-07 03:18:25 +0200

    forced test fail

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,7 +8,7 @@ public class MainTest {
         // Arrange
         int x = 5;
         int y = 3;
-        int expected = 8; // The expected result
+        int expected = 7; // The expected result
 
         // Act
         int result = Main.calculate(x, y);
