024b9c29f7218fe367cf73ea45edcd956d6837cf
spyros <spyrosgewdark@gmail.com>
2023-11-07 17:06:31 +0200

    fucked test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
     public void testCalculate() {
         // Arrange
         int x = 5;
-        int y = 3;
+        int y = 10;
         int expected = 8; // The expected result
 
         // Act
