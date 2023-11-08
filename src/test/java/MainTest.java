b07ffc553b141f9968b0613e4260d43e54aea524
Spyros_Gew <spyrosgewdark@gmail.com>
2023-11-08 12:48:00 +0200

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,23 @@
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+//TESTaaaaa
 public class MainTest {
 
 
+    @Test
+public class MainTest {
+    public void testCalculate() {
+        // Arrange
+        int x = 5;
+        int y = 3;
+        int expected = 8; // The expected result
+
+
+        // Act
+        int result = Main.calculate(x, y);
+
+
+        // Assert
+        assertEquals(expected, result);
+    }
 }
