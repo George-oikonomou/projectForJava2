8f363f29edf29a0ffd483ffb5d1d34e9afc3ed19
George-oikonomou <giorgos@workadu.com>
2023-11-10 17:29:57 +0200

    same thing with previous commit

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -4,49 +4,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ProjectTest
 {
-    private Project project;
-    @BeforeEach
-    void setUp()
-    {
-        project = new Project("title", "description", "date", "time", "deadline", true);
-    }
 
-    //@Test
-    public void testProject()
-    {
-        assertEquals("title", project.getTitle());
-        assertEquals("description", project.getDescription());
-        assertEquals("date", project.getDate());
-        assertEquals("time", project.getTime());
-        assertEquals("deadline", project.getDeadline());
-        assertTrue(project.isFinished());
-    }
-
-
-   // @Test
-    public void testGetDeadline()
-    {
-        assertEquals("deadline", project.getDeadline());
-    }
-
-  //  @Test
-    public void testSetDeadline()
-    {
-        assertEquals("deadline", project.getDeadline());
-        project.setDeadline("testDeadline");
-        assertEquals("testDeadline", project.getDeadline());
-    }
-
-   // @Test
-    public void testIsFinished()
-    {
-        assertTrue(project.isFinished());
-    }
-
-   // @Test
-    public void testSetFinished() {
-        assertTrue(project.isFinished());
-        project.setFinished(false);
-        assertFalse(project.isFinished());
-    }
 }
