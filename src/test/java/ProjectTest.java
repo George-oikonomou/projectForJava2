67d7a71c53028ff50d9b8f905d9f0684c17cee7b
George-oikonomou <giorgos@workadu.com>
2023-11-10 18:01:46 +0200

    testing something

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -5,22 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ProjectTest extends EventTest
 {
     private Project project;
-    @BeforeEach
-    void setUp()
-    {
-        project = new Project("title", "description", "date", "time", "deadline", true);
-    }
-
 
     @Test
     public void testGetDeadline()
     {
+        project = new Project("title", "description", "date", "time", "deadline", true);
         assertEquals("deadline", project.getDeadline());
     }
 
     @Test
     public void testSetDeadline()
     {
+        project = new Project("title", "description", "date", "time", "deadline", true);
+
         assertEquals("deadline", project.getDeadline());
         project.setDeadline("testDeadline");
         assertEquals("testDeadline", project.getDeadline());
@@ -29,11 +26,15 @@ public class ProjectTest extends EventTest
     @Test
     public void testIsFinished()
     {
+        project = new Project("title", "description", "date", "time", "deadline", true);
+
         assertTrue(project.isFinished());
     }
 
     @Test
     public void testSetFinished() {
+        project = new Project("title", "description", "date", "time", "deadline", true);
+
         assertTrue(project.isFinished());
         project.setFinished(false);
         assertFalse(project.isFinished());
