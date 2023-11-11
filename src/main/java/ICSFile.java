d0b0611a1d64a9eaa4bddd92d15d5b0b4a3e214a
spyros <spyrosgewdark@gmail.com>
2023-11-11 19:16:15 +0200

    created store file method

--- src/main/java/ICSFile.java
+++ src/main/java/ICSFile.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,20 +11,15 @@ import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
 
 public class ICSFile {
-    /**
-     * returns true if file exists
-     * @param fileName the file name
-     * @return returns its path
-     */
-    public static boolean ICSFilePath(String fileName) {
-        Path path = Paths.get(fileName);
 
-        if (Files.exists(path)) {
-            System.out.println("file exists");
-            return true;
-        }
-        return false;
+    ArrayList<Event> events;
+    String filePath;
+
+    public ICSFile(ArrayList<Event> events, String filePath) {
+        this.events = events;
+        this.filePath = filePath;
     }
+
 
     /**
      * method that will load the contents of the file int the array list of events
@@ -38,8 +35,64 @@ public class ICSFile {
 
     }
 
-    public static void StoreEvents(ArrayList<Event> events){
+    public void StoreEvents(ArrayList<Event> events){
+
+        try{
+            File file = new File(filePath);
+            if(file.createNewFile()){
+                System.out.println("File created " + file.getName());
+            }else {
+                System.out.println("File already exists. Overwriting");
+            }
+
+            FileWriter fileWriter = new FileWriter(file);
+
+            fileWriter.write("BEGIN:VCALENDAR\n");
+            fileWriter.write("VERSION:2.0\n");
+            fileWriter.write("PRODID:-//My Calendar//EN\n");
+
+            for (Event event : events){
+                fileWriter.write("BEGIN:VEVENT\n");
+                fileWriter.write("DTSTART:" + event.getDateTime() + "\n");
+                fileWriter.write("SUMMARY:" + event.getTitle() + "\n");
+                fileWriter.write("DESCRIPTION:" + event.getDescription() + "\n");
+
+                if (event instanceof Project){
+                    fileWriter.write("DEADLINE:" + ((Project) event).getDeadline());
+                    fileWriter.write("PROJECT_STATUS" + (((Project) event).isFinished() ? "Ongoing" : "finished"));
+                }
+
+                if (event instanceof Appointment){
+                    fileWriter.write("DURATION:" + ((Appointment) event).getDuration());
+                }
+
+                fileWriter.write("END:VEVENT\n");
+            }
+
+            fileWriter.write("END:VCALENDAR\n");
+
+            fileWriter.close();
+            System.out.println("successfully exported events to " + filePath);
+
+        }catch (IOException e){
+            System.out.println("error could not save file");
+        }
+
+    }
 
 
+    /**
+     * returns true if file exists
+     * @param fileName the file name
+     * @return returns its path
+     */
+    public static boolean ICSFilePath(String fileName) {
+        Path path = Paths.get(fileName);
+
+        if (Files.exists(path)) {
+            System.out.println("file exists");
+            return true;
+        }
+        return false;
     }
 }
