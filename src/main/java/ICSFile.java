e4e601e1b27abc2cd29d9dc0622ff221f9bc13a8
spyros <spyrosgewdark@gmail.com>
2023-11-14 01:31:32 +0200

    created a method that takes a string format of OurDateTime and turns it into OurDateTime objecet also creating Load events method not done but we might refactor code in dont want conflicts

--- src/main/java/ICSFile.java
+++ src/main/java/ICSFile.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,11 +17,58 @@ public class ICSFile {
      * in the calendar folder
      * fileName the path of the file we want to load
      */
-    public static void LoadEvents(String fileName) {
+    public static void LoadEvents(String filePath) {
+        ArrayList<Event> events = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            boolean insideEvent = false;
+
+            String title, summary, dateTime = null;
+            OurDateTime ourDateTime = null;
+            String type = null;
+
+            while ((line = br.readLine()) != null) {
+                if((line.startsWith("TYPE:"))){
+                    type = line.substring("TYPE:".length());
+                }
+                if(line.startsWith("BEGIN:VEVENT")){
+                    insideEvent = true;
+                }else if(line.startsWith("END:VEVENT")){
+                    insideEvent = false;
+                }else if(insideEvent && type != null){
+
+                        if(line.startsWith("DTSTART:")){
+                            dateTime = line.substring("DTSTART:".length());
+                            ourDateTime = OurDateTime.Functionality.StringToOurDateTime(dateTime);
+                        }else if(line.startsWith("SUMMARY:")){
+                            title = line.substring("SUMMARY:".length());
+                        }else if(line.startsWith("DESCRIPTION:")){
+                            summary = line.substring("DESCRIPTION:".length());
+                        }
+
+                        if(type.equals("PROJECT")){
+
+                            if(line.startsWith("DEADLINE:")){
+
+                            }
+                        }
+                    }
+                }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public void StoreEvents(ArrayList<Event> events){
+
+        public void StoreEvents(ArrayList<Event> events){
 
         try{
+            /*
+             * checks if the File Class found an existing file with the same name
+             * of if it did not find and created a new one
+             */
             File file = new File(filePath);
             if(file.createNewFile()){
                 System.out.println("File created " + file.getName());
@@ -32,32 +77,35 @@ public class ICSFile {
             }
 
             FileWriter fileWriter = new FileWriter(file);
-
+            /*
+                these contents must be at the start of every calendar
+             */
             fileWriter.write("BEGIN:VCALENDAR\n");
             fileWriter.write("VERSION:2.0\n");
             fileWriter.write("PRODID:-//My Calendar//EN\n");
             fileWriter.write("CALSCALE:GREGORIAN\n");
-
+                /*
+                we loop throw all of the events saving their information depending on the type of event
+                 */
             for (Event event : events){
                 fileWriter.write("BEGIN:VEVENT\n");
-                fileWriter.write("DTSTART:" + event.getDateTime() + "\n");
-                fileWriter.write("SUMMARY:" + event.getTitle() + "\n");
-                fileWriter.write("DESCRIPTION:" + event.getDescription() + "\n");
 
                 if (event instanceof Project){
-                    fileWriter.write("DEADLINE:" + ((Project) event).getDeadline() + "\n");
+                    fileWriter.write("TYPE:PROJECT\n");
+                    fileWriter.write("DEADLINE:" + ((Project) event).getDeadline().toString() + "\n");
                     fileWriter.write("PROJECT_STATUS" + (((Project) event).isFinished() ? "Finished" : "Ongoing") + "\n");
-                }
-
-                if (event instanceof Appointment){
+                } else if (event instanceof Appointment){
+                    fileWriter.write("TYPE:APPOINTMENT\n");
                     fileWriter.write("DURATION:" + ((Appointment) event).getDuration());
+                }else {
+                    fileWriter.write("TYPE:EVENT\n");
                 }
-
+                fileWriter.write("DTSTART:" + event.getDateTime().toString() + "\n");
+                fileWriter.write("SUMMARY:" + event.getTitle() + "\n");
+                fileWriter.write("DESCRIPTION:" + event.getDescription() + "\n");
                 fileWriter.write("END:VEVENT\n");
             }
-
             fileWriter.write("END:VCALENDAR\n");
-
             fileWriter.close();
             System.out.println("successfully exported events to " + filePath);
 
--- src/main/java/OurDateTime.java
+++ src/main/java/OurDateTime.java
@@ -9,27 +9,30 @@ public class OurDateTime {
     private String date;
     private String time;
 
-    public OurDateTime (int year, int month, int day, int hour, int minute) {
+    public OurDateTime(int year, int month, int day, int hour, int minute) {
         this.year = year;
         this.month = month;
         this.day = day;
         this.hour = hour;
         this.minute = minute;
-        this.date = Validate.date(year,month,day);
-        this.time = Validate.time(minute,hour);
+        this.date = Validate.date(year, month, day);
+        this.time = Validate.time(minute, hour);
     }
+
     /**
      * constructor that creates a OurDateTime but for events that do not have time
-     * @param year year of event
+     *
+     * @param year  year of event
      * @param month month of event
-     * @param day day of event
+     * @param day   day of event
      */
-    public OurDateTime(int year, int month, int day){
+    public OurDateTime(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
-        this.date = Validate.date(year,month,day);
+        this.date = Validate.date(year, month, day);
     }
+
     /**
      * constructor that creates a OurDateTime object that contains the current
      */
@@ -40,52 +43,75 @@ public class OurDateTime {
         this.day = teller.now().getDayOfMonth();
         this.hour = teller.now().getHour();
         this.minute = teller.now().getMinute();
-        this.date = Validate.date(year,month,day);
-        this.time = Validate.time(minute,hour);
+        this.date = Validate.date(year, month, day);
+        this.time = Validate.time(minute, hour);
     }
 
     public int getYear() {
         return year;
     }
+
     public void setYear(int year) {
         this.year = year;
     }
+
     public int getMonth() {
         return month;
     }
+
     public void setMonth(int month) {
         this.month = month;
     }
+
     public int getDay() {
         return day;
     }
+
     public void setDay(int day) {
         this.day = day;
     }
+
     public int getHour() {
         return hour;
     }
+
     public void setHour(int hour) {
         this.hour = hour;
     }
+
     public int getMinute() {
         return minute;
     }
+
     public void setMinute(int minute) {
         this.minute = minute;
     }
+
     public String getDate() {
         return date;
     }
+
     public void setDate(String date) {
         this.date = date;
     }
+
     public String getTime() {
         return time;
     }
+
     public void setTime(String time) {
         this.time = time;
     }
+
+    @Override
+    public String toString() {
+        if (getTime() == null){
+            return getDate();
+        }else {
+            return getDate() + " " +getTime();
+        }
+    }
+
 
     public static class Functionality{
         public static OurDateTime dateAndTime(boolean choice) {
@@ -112,6 +138,32 @@ public class OurDateTime {
             }
             //return object
             return new OurDateTime(year, month, day);
+        }
+
+        /*
+         * this is a method that will take the string of OurDateTime
+         * and will split it up and create new OurDateTime objects.
+         * I made it so we can extract the DateTime from the files
+         * and create a OurDateTime object with it.
+         */
+        public static OurDateTime StringToOurDateTime(String string){
+            int year , month ,day, hour, minutes;
+            String[] words = string.split(" ");
+            String date = words[0];
+            String[] dateParts = date.split("/");
+            year = Integer.parseInt(dateParts[0]);
+            month = Integer.parseInt(dateParts[1]);
+            day = Integer.parseInt(dateParts[2]);
+
+            if(words.length == 1){
+                return new OurDateTime(year,month,day);
+            }else{
+                String time = words[1];
+                String[] timeParts = time.split(":");
+                hour = Integer.parseInt(timeParts[0]);
+                minutes = Integer.parseInt(timeParts[1]);
+                return new OurDateTime(year,month,day,hour,minutes);
+            }
         }
     }
 }
